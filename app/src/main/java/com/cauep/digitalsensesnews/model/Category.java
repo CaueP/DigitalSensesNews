d3572c1dc0c85e8e361265d9378696d03e326032
Cauê Polimanti <caue.polimanti@gmail.com>
2017-05-27 23:58:38 -0300

    Added TODO to CategoryService

--- app/src/main/java/com/cauep/digitalsensesnews/model/Category.java
+++ app/src/main/java/com/cauep/digitalsensesnews/model/Category.java
@@ -1,10 +1,18 @@
 package com.cauep.digitalsensesnews.model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
 /**
  * Created by mario on 27/05/2017.
  */
 
-public class Category {
+public class Category implements Serializable{
+
+    @Expose
+    @SerializedName("title")
     private String title;
 
     public Category(String title){
--- app/src/main/java/com/cauep/digitalsensesnews/model/service/CategoryService.java
+++ app/src/main/java/com/cauep/digitalsensesnews/model/service/CategoryService.java
@@ -13,7 +13,7 @@ import retrofit2.http.GET;
  */
 
 public interface CategoryService {
-
+    // TODO: Transformar para Array de String
     @GET("news/categories")
     Call<ArrayList<Category>> getCategories();
 }
